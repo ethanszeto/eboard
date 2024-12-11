719bf75dfe981d9e476b8291464828edd9e0a35f
Ethan Szeto <ethan.szeto@gmail.com>
2024-12-11 17:54:59 -0500

    package lock fixes

--- frontend/components/Task/TaskDialogue.tsx
+++ frontend/components/Task/TaskDialogue.tsx
@@ -1,30 +1,23 @@
-import {
-    DialogDescription,
-    DialogHeader,
-    DialogTitle
-} from  "@shadcn/dialog"
-import { TaskStatus, Team } from "@components/types"
+import { DialogDescription, DialogHeader, DialogTitle } from "@shadcn/dialog";
+import { TaskStatus, Team } from "@components/types";
 
 type TaskDialogueProps = {
-    headline: string
-    description: string
-    status: TaskStatus
-    team: Team
-}
+  headline: string;
+  description: string;
+  status: TaskStatus;
+  team: Team;
+};
 
-export default function TaskDialogue({headline, description, status, team}: TaskDialogueProps) {
-    return (
-        <>
-            <DialogHeader>
-                <DialogTitle>{headline}</DialogTitle>
-            <DialogDescription>
-                {description}
-            </DialogDescription>
-            {team}
-            <br></br>
-            {status}
-            </DialogHeader>
-        </>
-    )
-    
+export default function TaskDialogue({ headline, description, status, team }: TaskDialogueProps) {
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>{headline}</DialogTitle>
+        <DialogDescription>{description}</DialogDescription>
+        {team}
+        <br></br>
+        {status}
+      </DialogHeader>
+    </>
+  );
 }
